e3d7e150fd493bd0a2ae5ee5f7825499ac2e49b1
Huaqiao Zhang <huaqiaoz@vmware.com>
2018-02-02 12:09:10 +0800

    Feat: add bind Enter key function  and add custom viewResolver function
    add bind Enter key function  and add custom viewResolver function
    
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- src/main/java/edgexfoundry/config/MvcConfig.java
+++ src/main/java/edgexfoundry/config/MvcConfig.java
@@ -1,19 +1,48 @@
+/*******************************************************************************
+ * Copyright © 2017-2018 VMware, Inc. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * @author: Huaqiao Zhang, <huaqiaoz@vmware.com>
+ * @version: 0.1.0
+ *******************************************************************************/
 package edgexfoundry.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 //@EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
 	
 		@Override
+		public void configureViewResolvers(ViewResolverRegistry registry) {
+			InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+			viewResolver.setPrefix("/");
+			viewResolver.setSuffix(".html");
+			//set current view resolver highest priority,
+			//if not, the spring web-app-context will traversal all default view-resolver until find the view.
+			viewResolver.setOrder(Ordered.HIGHEST_PRECEDENCE);
+			registry.viewResolver(viewResolver);
+		}
+	
+		@Override
 		public void addViewControllers(ViewControllerRegistry registry) {
-			registry.addViewController("/").setViewName("index.html");
+			registry.addViewController("/").setViewName("index");
 			//if you have declare '/login' path in Controller class,this will be invalid or conflict to Controller class.
-			registry.addViewController("/loginPage").setViewName("login.html"); 
+			registry.addViewController("/loginPage").setViewName("login"); 
 			registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
 		}	
 }
